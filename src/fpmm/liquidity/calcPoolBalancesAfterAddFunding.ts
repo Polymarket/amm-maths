bcc9a484fda05a1b751fde31b9e09ec87445c751
Tom French <tom@tomfren.ch>
2021-03-22 20:27:14 +0000

    refactor: switch to use calcAddFundingDepositedAmounts in calcPoolBalancesAfterAddFunding

--- src/fpmm/liquidity/calcPoolBalancesAfterAddFunding.ts
+++ src/fpmm/liquidity/calcPoolBalancesAfterAddFunding.ts
@@ -1,26 +1,15 @@
 import { BigNumber } from "@ethersproject/bignumber";
-import { calcAddFundingSendAmounts } from "./calcAddFundingSendAmounts";
+import { calcAddFundingDepositedAmounts } from "./calcAddFundingDepositedAmounts";
 
 /**
  * Compute the new pool balances in the Market Maker after adding `addedFunds` of collateral
  * @param addedFunds - the amount of collateral being added to the market maker as liquidity
  * @param initPoolBalances - the market maker's initial balances of outcome tokens
- * @param poolShareSupply - the total supply of liquidity pool tokens
  */
-export const calcPoolBalancesAfterAddFunding = (
-  addedFunds: BigNumber,
-  initPoolBalances: BigNumber[],
-  poolShareSupply: BigNumber,
-): BigNumber[] | null => {
-  if (poolShareSupply.eq(0)) {
-    return null;
-  }
+export const calcPoolBalancesAfterAddFunding = (addedFunds: BigNumber, initPoolBalances: BigNumber[]): BigNumber[] => {
+  const depositedAmounts = calcAddFundingDepositedAmounts(addedFunds, initPoolBalances);
 
-  const sendAmounts = calcAddFundingSendAmounts(addedFunds, initPoolBalances, poolShareSupply);
-
-  if (sendAmounts === null) return null;
-
-  const updatedBalances = initPoolBalances.map((balance, i) => balance.add(addedFunds).sub(sendAmounts[i]));
+  const updatedBalances = initPoolBalances.map((balance, i) => balance.add(depositedAmounts[i]));
 
   return updatedBalances;
 };
