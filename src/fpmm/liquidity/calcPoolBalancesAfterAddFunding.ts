a4d879de70e39b6eb8748615742e99d5fbcf8415
Sam <hatem@clearrain.xyz>
2021-03-22 14:52:33 -0500

    fix: return null if the pool share supply is Zero

--- src/fpmm/liquidity/calcPoolBalancesAfterAddFunding.ts
+++ src/fpmm/liquidity/calcPoolBalancesAfterAddFunding.ts
@@ -1,17 +1,26 @@
 import { BigNumber } from "@ethersproject/bignumber";
-import { Zero } from "@ethersproject/constants";
 import { calcAddFundingSendAmounts } from "./calcAddFundingSendAmounts";
 
+/**
+ * Compute the new pool balances in the Market Maker after adding `addedFunds` of collateral
+ * @param addedFunds - the amount of collateral being added to the market maker as liquidity
+ * @param initPoolBalances - the market maker's initial balances of outcome tokens
+ * @param poolShareSupply - the total supply of liquidity pool tokens
+ */
 export const calcPoolBalancesAfterAddFunding = (
   addedFunds: BigNumber,
   initPoolBalances: BigNumber[],
   poolShareSupply: BigNumber,
-): BigNumber[] => {
+): BigNumber[] | null => {
+  if (poolShareSupply.eq(0)) {
+    return null;
+  }
+
   const sendAmounts = calcAddFundingSendAmounts(addedFunds, initPoolBalances, poolShareSupply);
 
-  const updatedBalances = initPoolBalances.map((balance, i) =>
-    balance.add(addedFunds).sub((sendAmounts && sendAmounts[i]) || Zero),
-  );
+  if (sendAmounts === null) return null;
+
+  const updatedBalances = initPoolBalances.map((balance, i) => balance.add(addedFunds).sub(sendAmounts[i]));
 
   return updatedBalances;
 };
